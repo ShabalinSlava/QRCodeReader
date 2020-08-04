315e98d448ab015c37bb04dc802dbb3fb71f11ef
Shabalin Slava <svaznoy0309@gmail.com>
2020-08-04 17:31:08 +0800

    assignment callbak function qrcode object

---
+++ qrCodeScanner.js
@@ -0,0 +1,26 @@
+const qrcode = window.qrcode
+
+const video = document.createElement("video")
+const canvasElement = document.getElementById("qr-canvas")
+const canvas = canvasElement.getContent("2d")
+
+const qrResult = document.getElementById("qr-result")
+const outputData = document.getElementById("outputData")
+const btnScanQR = document.getElementById("btn-scan-qr")
+
+let scanning = false
+
+qrcode.callback = (res) => {
+  if (res) {
+    outputData.innerText = res
+    scanning = false
+
+    video.srcObject.getTracks().forEach(track => {
+      track.stop()
+    })
+
+    qrResult.hidden = false
+    btnScanQR.hidden = false
+    canvasElement.hidden = true
+  }
+}
